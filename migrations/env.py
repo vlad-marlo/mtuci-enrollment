cce3457d1fd3b392ee2fdce176fb235f66edf8f3
Vlad Marlo <vlad.marlo@ya.ru>
2024-01-26 18:44:08 +0300

    changed migrator configuration

--- migrations/env.py
+++ migrations/env.py
@@ -10,8 +10,6 @@ from alembic import context
 from src.config import DB_PASS, DB_HOST, DB_PORT, DB_USER, DB_NAME
 from src.core.models import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
@@ -21,22 +19,10 @@ config.set_section_option(section, "DB_USER", DB_USER)
 config.set_section_option(section, "DB_NAME", DB_NAME)
 config.set_section_option(section, "DB_PASS", DB_PASS)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
