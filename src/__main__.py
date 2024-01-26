6c6ce0e902ca7ea730379dad73a73a88427d3814
Vlad Marlo <vlad.marlo@ya.ru>
2024-01-26 22:26:57 +0300

    added users router

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from .api import routers
 
 app = FastAPI(title="TODO enrollment app")
+
+for router in routers:
+    app.include_router(router, prefix="/api/v1")
---
+++ src/api/__init__.py
@@ -0,0 +1,5 @@
+from .users import router as UserRouter
+
+routers = [
+    UserRouter,
+]
