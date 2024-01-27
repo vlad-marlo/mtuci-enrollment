bca01ba48ecb7354b5269e14ad1717901ab049da
Vlad Marlo <vlad.marlo@ya.ru>
2024-01-27 22:56:09 +0300

    fixed database connector

--- src/core/models/database.py
+++ src/core/models/database.py
@@ -38,12 +38,12 @@ class DatabaseHelper:
         )
         return session
 
-    async def session_dependency(self) -> Generator[AsyncSession]:
+    async def session_dependency(self) -> AsyncSession:
         async with self.session_factory() as session:
             yield session
             await session.close()
 
-    async def scoped_session_dependency(self) -> Generator[AsyncSession]:
+    async def scoped_session_dependency(self) -> AsyncSession:
         session = self.get_scoped_session()
         yield session
         await session.close()
