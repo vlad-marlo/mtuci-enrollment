94f3c7f3c4a06d63e82abcdabb1c5e95860f5dfb
Vlad Marlo <vlad.marlo@ya.ru>
2024-01-27 15:44:52 +0300

    fixed import names

---
+++ src/api/schemas/__init__.py
@@ -0,0 +1,13 @@
+
+from .user import (
+    User,
+    UserLoginRequest,
+    UserRegisterRequest,
+    UserShortInfo,
+)
+__all__ = (
+    "User",
+    "UserLoginRequest",
+    "UserRegisterRequest",
+    "UserShortInfo",
+)
