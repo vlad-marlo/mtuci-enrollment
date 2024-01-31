4eeedff649fdf4f3c35ee3f5d8882c10f6dd7239
Vlad Marlo <vlad.marlo@ya.ru>
2024-01-31 16:12:22 +0300

    added get all users service method

--- src/api/service/user.py
+++ src/api/service/user.py
@@ -1,6 +1,21 @@
 from src.api.storage import BaseUserStorage
+from src.api.schemas import (
+    GetManyUsersResponse,
+    User,
+)
 
 
 class UserService:
     def __init__(self, storage: BaseUserStorage):
         self.__storage = storage
+
+    async def get_all_users(self) -> GetManyUsersResponse:
+        users_from_db = await self.__storage.get_users()
+        result = [User.model_validate(x).short_info() for x in users_from_db]
+        response = GetManyUsersResponse(
+            result=result,
+            count=len(result),
+        )
+        return response
+
+
