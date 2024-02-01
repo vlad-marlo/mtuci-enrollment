56547861c6a807a4e4a68b15b2fb8f22bdf82436
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-01 14:26:25 +0300

    wrote ServiceException class

---
+++ src/api/exceptions.py
@@ -0,0 +1,27 @@
+from fastapi import status
+from structlog import get_logger
+
+logger = get_logger()
+
+
+class ServiceException(BaseException):
+    def __init__(
+            self,
+            code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail: str | None = None,
+            log: str | None = None
+    ):
+        self.__code = code
+        self.__detail = detail or "Internal server error"
+        if log:
+            logger.error(log, code=code, detail=detail)
+
+        super(self).__init__(self.__detail)
+
+    @property
+    def code(self) -> int:
+        return self.__code
+
+    @property
+    def detail(self) -> str:
+        return self.__detail
