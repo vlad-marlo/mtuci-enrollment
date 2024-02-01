c29afbd340e207e477791d8aaa9646f8a1745fe5
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-01 17:32:07 +0300

    deleted status field from Note model

--- src/core/models/note.py
+++ src/core/models/note.py
@@ -1,31 +1,28 @@
 from datetime import datetime
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, validates
+from sqlalchemy import func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
-from .mixins import UserMixin
 
 STATUS_CREATED = 0
 STATUS_PENDING_CHANGES = -1
 STATUS_APPROVED = 1
 
 
-class Note(Base, UserMixin):
+class Note(Base):
     __tablename__ = "notes"
     _user_back_populates = "notes"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     text: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(insert_default=func.now())
-    status: Mapped[int]
     is_deleted: Mapped[bool] = mapped_column(default=False)
-
-    @validates("status")
-    def validate_status(self, key, status):
-        if abs(status) > 1:
-            raise ValueError("status must be 0 or 1 or -1")
-        return status
+    created_by: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        unique=False,
+        nullable=False,
+    )
 
     def __str__(self) -> str:
         return (
