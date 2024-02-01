258308e673d7fa178edfc450ad4e59280f8091b7
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-01 17:50:35 +0300

    deleted redurant import

--- src/api/storage/sql/__init__.py
+++ src/api/storage/sql/__init__.py
@@ -1,12 +1,11 @@
 from .notes import NotesStorage
 from .revisions import RevisionStorage
-from .storage import Storage, get_storage
+from .storage import Storage
 from .user import UserStorage
 
 __all__ = (
     "NotesStorage",
     "RevisionStorage",
     "Storage",
-    "get_storage",
     "UserStorage",
 )
