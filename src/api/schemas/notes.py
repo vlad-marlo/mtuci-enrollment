b268195ab4e46c3bea7921a5910a8c8458b7bc29
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-01 17:57:18 +0300

    added base notes schemas

---
+++ src/api/schemas/notes.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    created_at: datetime
+
+
+class NoteCreationRequest(BaseModel):
+    text: str
+
+
+class NoteUpdateRequest(BaseModel):
+    text: str
+
+
+class NoteShortInfo(BaseModel):
+    id: int
+    text: str
+    created_at: datetime
+    created_by: int
